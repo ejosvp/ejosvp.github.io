5ac118b3be70ff83ed4d616d447d5c8cd265f7fe
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2016-09-04 15:32:36 -0500

    fix cs

--- blog/webpack.config.js
+++ blog/webpack.config.js
@@ -2,45 +2,45 @@ const webpack = require('webpack');
 const path = require('path');
 
 const PATHS = {
-    app: './src/index.js',
-    html: './src/index.html',
-    dist: path.join(__dirname, 'dist')
+  app: './src/index.js',
+  html: './src/index.html',
+  dist: path.join(__dirname, 'dist')
 };
 
 module.exports = {
-    entry: {
-        javascript: PATHS.app,
-        html: PATHS.html
-    },
-    output: {
-        path: PATHS.dist,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: PATHS.dist
-    },
-    eslint: {
-        emitWarning: false
-    },
-    module: {
-        preLoaders: [
-            {
-                test: /\.js?$/,
-                loaders: ["eslint-loader"],
-                exclude: /node_modules/
-            }
-        ],
-        loaders: [
-            {
-                test: /\.html$/,
-                loader: "file?name=[name].[ext]"
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: ["react-hot", "babel-loader"]
-            }
-        ]
-    }
+  entry: {
+    javascript: PATHS.app,
+    html: PATHS.html
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: PATHS.dist
+  },
+  eslint: {
+    emitWarning: false
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js?$/,
+        loaders: ["eslint-loader"],
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"]
+      }
+    ]
+  }
 };
