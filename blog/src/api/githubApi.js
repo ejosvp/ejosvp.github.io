4f13c9da11a9859527846a9f692ff67a9e2bf03f
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2016-09-04 16:55:18 -0500

    refactor api

--- blog/src/api/githubApi.js
+++ blog/src/api/githubApi.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 import config from '../config';
 
-function fetch(request, callback) {
-  axios.get(request)
-    .then(response => {
-      callback(response.data);
-    });
+export function getFileNames(callback) {
+  const request = `https://api.github.com/repos/${config.github.user}/${config.github.repo}/contents/posts`;
+  axios.get(request).then(response => callback(response.data));
 }
 
-export function getFiles(callback) {
-  const request = `https://api.github.com/repos/${config.github.user}/${config.github.repo}/contents/posts`;
-  fetch(request, callback);
-}
-
-export function getFile(callback) {
-  const request = `https://api.github.com/repos/${config.github.user}/${config.github.repo}/contents/posts`;
-  fetch(request, callback);
+export function getFile(name, callback) {
+  const request = `http://ejosvp.tech/posts/${name}`;
+  axios.request({
+    url: request,
+    responseType: 'text',
+  }).then(response => callback(response.data));
 }
