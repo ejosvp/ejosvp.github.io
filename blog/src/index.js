5e35c22b08e2b5f61dc68b38cea2b7eec243f098
Ernesto Jose Vargas Paz <ejosvp@gmail.com>
2016-09-04 15:33:35 -0500

    get and fill post list

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,2 +1,43 @@
-const foo = 'bar';
-foo.toUpperCase();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './components/Menu';
+import PostList from './components/PostList';
+import * as api from './api/githubApi';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = ({
+      posts: [],
+    });
+    this.getPosts = this.getPosts.bind(this);
+    this.processPosts = this.processPosts.bind(this);
+
+    this.getPosts();
+  }
+
+  getPosts() {
+    api.getFiles(this.processPosts);
+  }
+
+  processPosts(payload) {
+    this.setState({
+      posts: payload,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu />
+        <PostList posts={this.state.posts} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
